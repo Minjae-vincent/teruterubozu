4a3f14e5941ba5251d0f537cce696b007ace3ccf
mj.kim <alswo9853@gmail.com>
2023-07-05 23:11:40 +0900

    feat: 날씨 API call 추가

--- backend/src/main/java/com/teruterubozu/service/weatherService.java
+++ backend/src/main/java/com/teruterubozu/service/weatherService.java
@@ -1,9 +1,17 @@
 package com.teruterubozu.service;
 
+import java.util.Date;
+import java.util.Map;
+
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.stereotype.Service;
-
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.io.BufferedReader;
 import com.teruterubozu.domain.weatherDTO;
-
 import lombok.extern.slf4j.Slf4j;
 
 @Service
@@ -46,8 +54,58 @@ public class weatherService {
         gpt.setLat(Math.floor(ra * Math.sin(theta) + XO + 0.5));
         gpt.setLon(Math.floor(ro - ra * Math.cos(theta) + YO + 0.5));
 
-        log.info(gpt.toString());
-
         return gpt;
+    }
+    public Map<String, Object> getWeather(weatherDTO grid) {
+        Date tmp = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+        String base_date = format.format(tmp);
+        format = new SimpleDateFormat("HHmm");
+        String base_time = format.format(tmp);
+
+        base_time = String.valueOf(Integer.parseInt(base_time.substring(0, 2)) - 1) + "00";
+                
+        try{
+            StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst"); /*URL*/
+            urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=" + "eM2GI%2FKiuCJfMm9yc1w%2FSDnIXO204g2EwTn2ZHYbJIIW364iAYg56QvjLWIhpqrsgv9iUPr15YK6SQQP9Wbimg%3D%3D"); /*Service Key*/
+            urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
+            urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("1000", "UTF-8")); /*한 페이지 결과 수*/
+            urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*요청자료형식(XML/JSON) Default: XML*/
+            urlBuilder.append("&" + URLEncoder.encode("base_date","UTF-8") + "=" + URLEncoder.encode(base_date, "UTF-8")); /*‘21년 6월 28일 발표*/
+            urlBuilder.append("&" + URLEncoder.encode("base_time","UTF-8") + "=" + URLEncoder.encode(base_time, "UTF-8")); /*06시30분 발표(30분 단위)*/
+            urlBuilder.append("&" + URLEncoder.encode("nx","UTF-8") + "=" + URLEncoder.encode(String.valueOf((int) grid.getLat()), "UTF-8")); /*예보지점 X 좌표값*/
+            urlBuilder.append("&" + URLEncoder.encode("ny","UTF-8") + "=" + URLEncoder.encode(String.valueOf((int) grid.getLon()), "UTF-8")); /*예보지점 Y 좌표값*/
+
+            URL url = new URL(urlBuilder.toString());
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-type", "application/json");
+            BufferedReader rd;
+            if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+                rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            } else {
+                rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+            }
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                sb.append(line);
+            }
+            rd.close();
+            conn.disconnect();
+
+            JSONParser jsonParser = new JSONParser(sb.toString());
+            Map<String, Object> weather = jsonParser.parseObject();
+            
+            log.info(urlBuilder.toString());
+            log.info(weather.toString());
+            return weather;
+
+            
+        } catch(Exception e) {
+            log.info(e.getMessage());
+        }
+        
+        return null;
     }
 }
