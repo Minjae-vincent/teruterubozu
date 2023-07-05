0e5f6101c4fc424c4adc92be8d7558d70c329eb6
mj.kim <alswo9853@gmail.com>
2023-07-05 18:16:21 +0900

    feat: 위경도 격자값 변환 로직 구현

---
+++ backend/src/main/java/com/teruterubozu/controller/WeatherController.java
@@ -0,0 +1,23 @@
+package com.teruterubozu.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.teruterubozu.domain.weatherDTO;
+import com.teruterubozu.service.weatherService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class WeatherController {
+    @Autowired
+    private weatherService weatherService;    
+    @PostMapping("/api/weather")
+    public void postWeather(@RequestBody weatherDTO weatherDTO) {
+        weatherService.computeGrid(weatherDTO.getLat(), weatherDTO.getLon());
+    }
+
+}
---
+++ backend/src/main/java/com/teruterubozu/domain/weatherDTO.java
@@ -0,0 +1,9 @@
+package com.teruterubozu.domain;
+
+import lombok.Data;
+
+@Data
+public class weatherDTO {
+    private double lat;
+    private double lon;
+}
