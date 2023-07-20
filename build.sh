22aa6a5d9595a1a701677023dbb31ab4e442250c
mj.kim <alswo9853@gmail.com>
2023-07-20 17:00:41 +0900

    fix: fix build shell script to run correctely

--- build.sh
+++ build.sh
@@ -1,8 +1,20 @@
-cd ./frontend
-npm run build
+cd ./backend
 
-mv ./dist/* ../backend/src/main/resources/static
+# remove static folder if exists
+mvn clean
+
+# make static folder where frontend build will be copied
+mkdir -p target/classes/static
+
+cd ../frontend
+
+# install frontend dependencies
+npm install
+
+# build frontend
+npm run build -- --dest ../backend/target/classes/static
 
 cd ../backend
 
+# build backend
 mvn package -DskipTests
